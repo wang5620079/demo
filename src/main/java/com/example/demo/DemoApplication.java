0c190aa9e09e2224d911c13ffc5a3cf041420950
wang5620079 <wang5620079@163.com>
2019-03-10 15:07:49 +0800

    加入log4j

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class DemoApplication {
+	static Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	@RequestMapping("")
+	public String index(){
+//		System.out.println("get request!");
+		logger.info("get index request!");
+		return "Test index OK!";
+	}
 	@RequestMapping("hi")
 	public String hi(){
+//		System.out.println("get request!");
+		logger.info("get request!");
 		return "Test OK!";
 	}
 }
